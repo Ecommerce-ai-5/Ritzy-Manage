fe12fb495a009cef083fb043c9a77452a56d8cd3
Ecommerce-ai-5 <omarmohmedamin1000@gmail.com>
2024-07-12 23:44:43 +0300

    Add files via upload

---
+++ js/inithialize-firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBEYFmAr1ey10L1l9uvuhOnYehAp-RAOls",
+  authDomain: "ritzystock-2f0e5.firebaseapp.com",
+  databaseURL: "https://ritzystock-2f0e5-default-rtdb.firebaseio.com",
+  projectId: "ritzystock-2f0e5",
+  storageBucket: "ritzystock-2f0e5.appspot.com",
+  messagingSenderId: "1023121556799",
+  appId: "1:1023121556799:web:7e4a2a4247fb8825b7ff8a",
+  measurementId: "G-BMPLJ1YHST",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
